e03b016ce97a8db6d916eaceaba4c7c0b5f2aa94
Sheng Pang <shengdevelop@gmail.com>
2026-06-09 11:51:16 -0700

    fix project test path

--- extension/playwright.config.ts
+++ extension/playwright.config.ts
@@ -3,7 +3,10 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   testDir: './src/e2e',
   timeout: 60000,
-  use: {
-    headless: true,
-  },
+  projects: [
+    {
+      name: 'electron',
+      use: {},
+    },
+  ],
 });
